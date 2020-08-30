a0b17746cb7f8a86c486a9e99b29cd0a61db5453
Mortemis <artyom.paskin@gmail.com>
2020-08-30 19:19:59 +0400

    Style tweaks

--- src/controllers/control.ts
+++ src/controllers/control.ts
@@ -4,16 +4,20 @@ import robot = require('../robot');
 
 export const fwd = (req: Request, res: Response) => {
     robot.moveFwd();
+    res.status(200).json({msg: 'ok'});
 }
 
 export const back = (req: Request, res: Response) => {
     robot.moveBack();
+    res.status(200).json({msg: 'ok'});
 }
 
 export const left = (req: Request, res: Response) => {
     robot.rotateLeft();
+    res.status(200).json({msg: 'ok'});
 }
 
 export const right = (req: Request, res: Response) => {
     robot.rotateRight();
+    res.status(200).json({msg: 'ok'});
 }
